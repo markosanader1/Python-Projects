75e4a29a675d93588767f9b4f0d05f662ec13003
markosanader1 <80037713+markosanader1@users.noreply.github.com>
2021-04-08 13:53:22 +0200

    Add files via upload

---
+++ projekat.main.py
@@ -0,0 +1,56 @@
+import os
+import requests
+from datetime import datetime
+
+APP_ID = os.environ["APP_ID"]
+APP_KEY = os.environ["APP_KEY"]
+SECRET_TOKEN = os.environ["SECRET_TOKEN"]
+
+url = "https://trackapi.nutritionix.com/v2/natural/exercise"
+unos = input('What did u do today?')
+
+headers = {"x-app-id": APP_ID,
+"x-app-key" : APP_KEY,
+"Content-Type": "application/json",
+
+}
+body={
+ "query":unos,
+ "gender":"male",
+ "weight_kg":85.00,
+ "height_cm":187.00,
+ "age":28
+}
+response = requests.post(url=url, headers=headers, json=body)
+response.raise_for_status()
+data = response.json()
+print(data)
+
+today_date = datetime.now().strftime("%d/%m/%Y")
+now_time = datetime.now().strftime("%X")
+# exercise = data['exercises'][0]['user_input']
+# duration = data['exercises'][0]['met']
+# calories = data['exercises'][0]['nf_calories']
+
+vezbe=[_['name'].title() for _ in data["exercises"]]
+print(vezbe)
+print(data["exercises"])
+sheety_url = "https://api.sheety.co/f8f57d2ac1288732b6a288759601be87/copyOfMyWorkouts/workouts"
+for _ in data["exercises"]:
+ dodatak = {
+   "workout":
+    {
+    "date": today_date,
+    "time": now_time,
+    "exercise": _['name'],
+    "duration":_['duration_min'],
+    "calories":_['nf_calories']
+
+  }
+ }
+
+ bearer_header = {"Authorization": f"Bearer {SECRET_TOKEN}",
+                  "Content-Type":"application/json",
+                  }
+ response = requests.post(url=sheety_url, json=dodatak, headers=bearer_header)
+ print(response.text)
