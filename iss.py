7acf31314033b2ccd3518559270e3f7e81151929
markosanader1 <80037713+markosanader1@users.noreply.github.com>
2021-04-08 13:44:02 +0200

    Update iss.py

--- iss.py
+++ iss.py
@@ -1,13 +1,19 @@
+'''This is a code that sends API request to http://api.open-notify.org/iss-now.json, for information about current position of International Space Station (ISS). 
+Code than compares current latitude and longitude of the ISS and cordinates of the chosen plase(MY_LAT and MY_LONG are set to the cordinates of Belgrade, Serbia).
+The code also requests the information about when the sun rises and sets at current day.It sends an API request to "https://api.sunrise-sunset.org/json" for information 
+about sunrise and sunset.If the ISS is close to current position, and if it is night, the code uses smptplib module to send an email to the user,informing him that ISS is close.''' 
+
 import requests
 from datetime import datetime
 import smtplib
 import time
+import os
 
 MY_LAT = 44.786568 # Your latitude
 MY_LONG = 20.448921 # Your longitude
 
-MY_EMAIL = "m93srbija@gmail.com"
-MY_PASSWORD = 'makotonagano'
+MY_EMAIL = os.environ.get("EMAIL")
+MY_PASSWORD = os.environ.get("PASSWORD")
 
 response = requests.get(url="http://api.open-notify.org/iss-now.json")
 response.raise_for_status()
